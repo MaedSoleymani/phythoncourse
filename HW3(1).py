8c4ad1358fb657487942cf92c41f3fd039f5c3f9
MaedSoleymani <99656527+MaedSoleymani@users.noreply.github.com>
2022-03-07 00:38:56 +0330

    Add files via upload

---
+++ HW3(1).py
@@ -0,0 +1,13 @@
+income = int(input('please inter number of income;'))
+
+if income <1000 :
+    taxation = income
+if 1000<=income<=2500 :
+    taxation = income-((income*10)//100)
+if 2500<income<=4000 :
+    taxation = income-((income*15)//100)
+if 4000<income<=6000 :
+    taxation = income-((income*20)//100)
+if income<8000 :
+    taxation = income-((income*30)//100)
+print(taxation)
---
+++ HW3(3).py
@@ -0,0 +1,9 @@
+a = input('First number : ')
+b = input('Second number : ')
+c = input('Third number : ')
+d = input('fourth number :')
+e = input('fifth number ; ')
+if a == b or a == c or a==d or a==e or b==c or b==d or b==e or c==d or c==e or d==e:
+ print ('duplicates')
+else:
+ print ('all uniqe')
